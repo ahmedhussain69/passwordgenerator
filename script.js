4e3899ba905fc0ca9689a2cfb8cd9e869c4c7ab7
Ahmed Hussain <a_hussain32@yahoo.co.uk>
2022-12-02 18:51:02 +0000

    Added password validation, user selection input code

--- script.js
+++ script.js
@@ -88,6 +88,100 @@ var specialCharacters = [
     'Z'
   ];
 
+  // Created a function for password length validation
+
+let passwordValidation = (userInput) => {
+    if (userInput === "") {
+      alert('Cancel pressed. Please Re-try')
+      return false;
+    } else if (userInput < 10 || userInput > 64) {
+      alert('Re-try again. Selection needs to between numbers, 10 - 64')
+      return false;
+    } else if (isNaN(userInput)) {
+      alert('Numeric input only allowed')
+      return false;
+    }
+    return true;
+  
+  };
+
+  // Function for User password options and Random Element
+let userSelectionInput = (passwordLength) => {
+    let passwordArray = [];
+    let isLow = confirm("Would you like to include lower case letters?");
+    let isUp = confirm('Would you like to include upper case letters?');
+    let isNum = confirm('Would you like to include numbers?');
+    let isSpec = confirm('Would you like to include special characters?');
+  
+    let randomSelectionArray = []
+  
+    // If no option is selected
+    if (!isLow && !isUp && !isNum && !isSpec) {
+      alert('You must select at least one option')
+      return false;
+  
+    } else if (isLow && isUp && isNum && isSpec) {
+      
+      //Four options true.
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters, upperCasedCharacters, numericCharacters, specialCharacters)
+  
+      //Three options true.
+    } else if (isLow && isUp && isNum) {
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters, upperCasedCharacters, numericCharacters);
+    } else if (isLow && isUp && isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters, upperCasedCharacters, specialCharacters);
+    } else if (isLow && isNum && isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters, numericCharacters, specialCharacters);
+    } else if (isUp && isNum && isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(upperCasedCharacters, numericCharacters, specialCharacters);
+  
+      //Two options true.
+    } else if (isLow && isUp) {
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters, upperCasedCharacters);
+    } else if (isLow && isNum) {
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters, numericCharacters);
+    } else if (isLow && isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters, specialCharacters);
+    } else if (isUp && isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(upperCasedCharacters, specialCharacters);
+    } else if (isUp && isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(upperCasedCharacters, specialCharacters);
+    } else if (isNum && isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(numericCharacters, specialCharacters);
+    } else if (isUp && isNum) {
+      randomSelectionArray = randomSelectionArray.concat(upperCasedCharacters, numericCharacters);
+  
+  
+      //One option true.
+    } else if (isLow) {
+      randomSelectionArray = randomSelectionArray.concat(lowerCasedCharacters);
+    } else if (isUp) {
+      randomSelectionArray = randomSelectionArray.concat(upperCasedCharacters);
+    } else if (isNum) {
+      randomSelectionArray = randomSelectionArray.concat(numericCharacters);
+    } else if (isSpec) {
+      randomSelectionArray = randomSelectionArray.concat(specialCharacters);
+    }
+   
+    for (var i = 0; i < passwordLength; i++) {
+      let randomCharacter = Math.floor(Math.random() * randomSelectionArray.length);
+      passwordArray.push(randomSelectionArray[randomCharacter])
+    }
+    // Code below remove commas and quotation marks.
+    return passwordArray.join("");
+  }
+
+// Function to generate password with user input
+let generatePassword = () => {
+    let passwordLength = prompt('Please enter a numerical password length that is between 10 and 64 characters long')
+    if (passwordValidation(passwordLength)) {
+      return (userSelectionInput(passwordLength))
+    } else {
+      console.log('Invalid Selection')
+    }
+  
+  }
+
   // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
